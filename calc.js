299d5552c4624fca51631cd44c8b21732ecbce61
tim <t4i5m6@yahoo.com.tw>
2016-08-04 13:28:59 +0800

    add all

---
+++ calc.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+	console.log( a + ' + ' + b + ' = ');
+	return a + b;
+}
+
+
+function minus(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function division(a, b) {
+	return a / b;
+}
+
+module.exports = {
+	add: add,
+	minus: minus,
+	multiply: multiply,
+	division: division,
+};
